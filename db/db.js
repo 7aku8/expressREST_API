1890bcefc9267e48f292c92b48f838b4a5d4ecac
Jakub Wolak <jakubwolak.123@gmail.com>
2020-08-01 16:03:35 +0200

    improve db connection

--- db/db.js
+++ db/db.js
@@ -7,3 +7,12 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, pr
     host: process.env.DB_HOST,
     dialect: process.env.DB_CONNECTION
 });
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established succesfully');
+} catch (err) {
+    console.log('Unable to connect to database: ', err);
+}
+
+module.exports = sequelize;
