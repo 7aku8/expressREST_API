a2769036edd759e47823fa549e3f68963c33524c
Jakub Wolak <jakubwolak.123@gmail.com>
2020-08-03 18:23:23 +0200

    new rest api methods

--- routes/index.js
+++ routes/index.js
@@ -6,73 +6,121 @@ const uuid = require('uuid');
 
 // add new post
 router.post('/', (req, res) => {
-  const {
-    title,
-    lead,
-    content
-  } = req.body;
+  const newPost = {
+    id: uuid.v4(),
+    title: req.body.title,
+    lead: req.body.lead,
+    content: req.body.content
+  }
 
-  let status = 401;
-  let message = ``;
-
-  Post.create({
-      id: uuid.v4(),
-      title: title,
-      lead: lead,
-      content: content,
-    })
+  Post.create(newPost)
     .then(() => {
-      status = 200;
-      message = `Post created succesfully`;
-
-      res.status(status).json({
-        msg: message
-      });
+      res.status(200).json({
+        "msg": "Post has been created succesfully"
+      })
     })
     .catch((err) => {
-      status = 401;
-      message = `Error: ${err}`;
-
-      res.status(status).json({
-        msg: message
-      });
+      res.status(401).json({
+        "msg": `Error: Database problem occured, status code: ${err.status}`
+      })
     })
-
 });
 
 // edit existing post
-router.put('/:id', (req, res) => {
-  res.json({
-    msg: "edit post"
-  });
+router.patch('/:id', (req, res) => {
+  const id = req.params.id;
+  const [
+    key,
+    value
+  ] = Object.entries(req.body)[0];
+
+  Post.update({
+      where: {
+        id: id
+      },
+      key: value
+    }).then(() => {
+      res.status(200).json({
+        "msg": "Post has been updated successfully"
+      })
+    })
+    .catch((err) => {
+      res.status(401).json({
+        "msg": `Error: Something went wrong, status code: ${err.status}`
+      })
+    })
 });
 
 // show single post
 router.get('/:id', (req, res) => {
-  res.json({
-    msg: "show single post"
-  });
+  const id = req.params.id;
+
+  Post.findOne({
+      where: {
+        id: id
+      }
+    })
+    .then((post) => {
+      res.status(200).json({
+        "msg": post
+      })
+    })
+    .catch((err) => {
+      res.status(401).json({
+        "msg": `Post with hiven id has not been found, status code ${err.code}`
+      })
+    })
 });
 
 // show all posts
 router.get('/', (req, res) => {
-  res.json({
-    msg: "show all posts"
-  });
+  Post.findAll()
+    .then((posts) => {
+      res.status(200).json({
+        "msg": posts
+      })
+    })
+    .catch((err) => {
+      res.status(401).json({
+        "msg": `Posts have not been found, status code ${err.code}`
+      })
+    })
 });
 
 // delete single post
 router.delete('/:id', (req, res) => {
-  res.json({
-    msg: "delete single post"
-  });
+  const id = req.params.id;
+
+  Post.destroy({
+      where: {
+        id: id
+      }
+    }).then(() => {
+      res.status(200).json({
+        "msg": "Post has been deleted succesfully"
+      })
+    })
+    .catch((err) => {
+      res.status(401).json({
+        "msg": `Error: Invalid post's id, status code ${err.status}`
+      })
+    })
 });
 
 // delete all posts
 router.delete('/', (req, res) => {
-  res.json({
-    msg: "delete multiple posts"
-  });
+  Post.destroy({
+      truncate: true
+    }).then(() => {
+      res.status(200).json({
+        "msg": "Posts have been deleted succesfully"
+      })
+    })
+    .catch((err) => {
+      res.status(401).json({
+        "msg": `Error: Unable to delete all posts ${err.status}`
+      })
+    })
 });
 
 module.exports = router;
