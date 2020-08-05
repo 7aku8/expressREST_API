592d9faa0ca91846c50b950d37c224615c162de4
Jakub Wolak <jakubwolak.123@gmail.com>
2020-08-05 02:39:20 +0200

    change response structure, remove status_code from response

--- utils/responseUtils.js
+++ utils/responseUtils.js
@@ -1,50 +1,42 @@
-const {
-    RSA_NO_PADDING
-} = require("constants");
-
 const success = (res, message, posts) => {
-    res.status(200).json({
-        "success": true,
-        "message": message,
-        "data": {
-            "posts": posts
-        }
-    })
-    return;
+  res.status(200).json({
+    success: true,
+    message: message,
+    data: {
+      posts: posts,
+    },
+  });
+  return;
 };
 
 const failure = (res, status_code, message) => {
-    res.status(status_code).json({
-        "success": false,
-        "message": message,
-        "status_code": status_code,
-        "data": {}
-    })
-    return;
+  res.status(status_code).json({
+    success: false,
+    message: message,
+  });
+  return;
 };
 
 const successUser = (res, token) => {
-    res.status(200).json({
-        "success": true,
-        "message": "You are logged in",
-        "token": token,
-    })
-    return;
-}
+  res.status(200).json({
+    success: true,
+    message: "You are logged in",
+    token: token,
+  });
+  return;
+};
 
-const failureUser = (res, status_code = 400, message) => {
-    res.status(status_code).json({
-        "success": false,
-        "status": status_code,
-        "message": message
-    })
-    return;
-}
-
+const failureUser = (res, status_code, message) => {
+  res.status(status_code).json({
+    success: false,
+    message: message,
+  });
+  return;
+};
 
 module.exports = {
-    success,
-    failure,
-    successUser,
-    failureUser
+  success,
+  failure,
+  successUser,
+  failureUser,
 };
